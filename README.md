435b21bf3fc19aff09114b716aabd4a4f01a8b7b
Dohun-choi <hallow2546@gmail.com>
2024-08-20 07:06:31 +0900

    [Feat] 1.0.0 - H2, H3 태그를 정리하여 오른쪽 빈 공간에 테이블로 표시(TOC) - TOC에 표시된 제목을 누르면 해당 위치로 이동 - TOC에 현재 보고 있는 위치 표시

---
+++ README.md
@@ -0,0 +1,43 @@
+# TOC-tistory-Square
+
+티스토리 블로그 Square 스킨의 TOC(Table Of Contents)
+
+## 기능
+
+- H2, H3 태그를 정리하여 오른쪽 빈 공간에 테이블로 표시(TOC)
+- TOC에 표시된 제목을 누르면 해당 위치로 이동
+- TOC에 현재 보고 있는 위치 표시
+
+## 적용
+
+Squre 스킨 전용: 해당 위치에 `<div id="table-of-contents"></div>` 코드 삽입
+
+```html
+<div id="content">
+  <div id="content-inner"></div>
+  <!-- content-inner close -->
+  <div id="table-of-contents"></div>
+  <!-- 삽입 -->
+</div>
+<!-- content close -->
+```
+
+toc.min.js와 toc.min.css를 업로드
+
+## 수정 - 선택사항
+
+최초 clone 후
+
+```bash
+npm install
+```
+
+TOC.js 혹은 TOC.css 수정 후
+
+```bash
+npm run minifyTOC
+```
+
+toc.min.js와 toc.min.css 생성
+
+- console과 debugger 및 주석이 지워진 작은 js, css 파일로 변경
---
+++ TOC.js
@@ -0,0 +1,148 @@
+// @ts-check
+
+const tagHref = {
+  h2Tag(hTag) {
+    return `h2-${hTag.offsetTop}`;
+  },
+  h3Tag(hTag) {
+    return `h3-${hTag.offsetTop}`;
+  },
+};
+
+const contentId = "article-view";
+
+window.addEventListener("scroll", TOCHighlight);
+
+const articleContents = document.getElementById(contentId);
+
+(function makeToc(articleContents) {
+  if (!articleContents) return;
+
+  const hElements = articleContents.querySelectorAll("h2, h3");
+  const tocContainer = document.querySelector("#table-of-contents");
+
+  const tocList = document.createElement("ul");
+  tocContainer?.appendChild(tocList);
+  tocList.id = "TOC-list";
+
+  let currentH3List = null;
+
+  hElements.forEach((hTag) => {
+    if (hTag.tagName === "H2") {
+      hTag.id = tagHref.h2Tag(hTag);
+
+      const currentH2Item = document.createElement("li");
+      const h2Link = document.createElement("a");
+      h2Link.textContent = hTag.textContent;
+      h2Link.href = `#${tagHref.h2Tag(hTag)}`;
+      h2Link.id = `TOC-${tagHref.h2Tag(hTag)}`;
+      h2Link.classList.add("h2-TOC");
+
+      currentH2Item.appendChild(h2Link);
+
+      currentH3List = document.createElement("ul");
+      currentH2Item.appendChild(currentH3List);
+      tocList.appendChild(currentH2Item);
+    } else if (hTag.tagName === "H3") {
+      hTag.id = tagHref.h3Tag(hTag);
+
+      if (currentH3List) {
+        const listItem = document.createElement("li");
+        const h3Link = document.createElement("a");
+        h3Link.textContent = hTag.textContent;
+        h3Link.href = `#${tagHref.h3Tag(hTag)}`;
+        h3Link.id = `TOC-${tagHref.h3Tag(hTag)}`;
+        h3Link.classList.add("h3-TOC");
+
+        listItem.appendChild(h3Link);
+        currentH3List.appendChild(listItem);
+      }
+    }
+  });
+})(articleContents);
+
+function TOCHighlight() {
+  const className = "active_TOC";
+
+  const articleContents = document.getElementById(contentId);
+  if (!articleContents) return;
+  const hElements = Array.from(articleContents.querySelectorAll("h2, h3"));
+
+  const viewportTop = window.scrollY;
+  const viewportMiddle = window.scrollY + window.innerHeight / 2;
+
+  const elementsInRange = hElements.filter((el) => {
+    const rect = el.getBoundingClientRect();
+    const elementTop = rect.top + window.scrollY;
+    const elementBottom = elementTop + rect.height;
+
+    return elementTop <= viewportMiddle && elementBottom >= viewportTop;
+  });
+
+  let closest;
+
+  if (elementsInRange.length > 0) {
+    // 중간까지 보이는 요소가 존재하면 그 중에서 첫 번째 요소를 반환
+    closest = elementsInRange[0];
+  } else {
+    // 화면 위쪽에 있는 가장 마지막 요소를 찾음
+    const elementsAboveViewport = hElements.filter((el) => {
+      const rect = el.getBoundingClientRect();
+      const elementTop = rect.top + window.scrollY;
+      const elementBottom = elementTop + rect.height;
+
+      return elementBottom < viewportTop;
+    });
+
+    if (elementsAboveViewport.length > 0) {
+      // 가장 마지막 요소를 반환
+      closest = elementsAboveViewport[elementsAboveViewport.length - 1];
+    }
+  }
+
+  const toActive = getTocElement(closest);
+
+  if (toActive) {
+    hElements.forEach((e) => {
+      getTocElement(e)?.classList.remove(className);
+    });
+    toActive.classList.add(className);
+    scrollToCenter(toActive, "table-of-contents");
+  }
+}
+
+function scrollToCenter(element, containerId) {
+  const container = document.getElementById(containerId);
+
+  if (!container) return;
+
+  const containerRect = container.getBoundingClientRect();
+  const elementRect = element.getBoundingClientRect();
+
+  const elementCenter = elementRect.top - containerRect.top + container.scrollTop + elementRect.height / 2;
+  const containerCenter = containerRect.height / 2;
+  const scrollTop = elementCenter - containerCenter;
+
+  container.scrollTo({
+    top: scrollTop,
+    behavior: "smooth",
+  });
+}
+
+/**
+ *
+ * @param {Element | undefined} articleHTag
+ * @returns {HTMLElement | null}
+ */
+function getTocElement(articleHTag) {
+  let element;
+  if (!articleHTag) return null;
+
+  if (articleHTag.tagName === "H2") {
+    element = document.getElementById(`TOC-${articleHTag.id}`);
+  } else {
+    element = document.getElementById(`TOC-${articleHTag.id}`);
+  }
+
+  return element;
+}
